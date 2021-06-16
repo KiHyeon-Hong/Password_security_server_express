a708bcf4bf9b6195b9e369ae30ff1b7f0bf14899
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-16 16:51:37 +0900

    file download test

--- mainController.js
+++ mainController.js
@@ -8,13 +8,19 @@ const passwordSecurityServer = require('@kihyeon-hong/password_security_server')
 const app = express();
 
 app.get('/Test', (req, res) => {
-	console.log('request and response test');
-	res.send(new passwordSecurityServer.passwordSecurityServer.Test().test());
+	// console.log('request and response test');
+	// res.send(new passwordSecurityServer.passwordSecurityServer.Test().test());
 });
 
 app.get('/Temp', (req, res) => {
 	res.send("temp code");
-  });
+});
+
+app.get('/download', (req, res, next) => {
+	const name = 'weights.bin';  
+	res.setHeader('Content-Disposition', `attachment; filename=${name}`); // 이게 핵심 
+	res.sendFile(__dirname + '/weights.bin');
+});  
 
 app.listen(65001, () => {
 	request.get(
