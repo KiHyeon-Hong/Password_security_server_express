25e1f8d0a936912d89c38f4f6182c45906830606
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-21 20:11:48 +0900

    model Train Test

--- mainController.js
+++ mainController.js
@@ -3,24 +3,43 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const request = require('request');
 
-// const passwordSecurityServer = require('./PasswordSecurity');
-const passwordSecurityServer = require('@kihyeon-hong/password_security_server');
+const passwordSecurityServer = require('./PasswordSecurity');
+// const passwordSecurityServer = require('@kihyeon-hong/password_security_server');
 const app = express();
 
 app.get('/Test', (req, res) => {
-	// console.log('request and response test');
-	// res.send(new passwordSecurityServer.passwordSecurityServer.Test().test());
+	console.log('request and response test');
+	res.send(new passwordSecurityServer.passwordSecurityServer.Test().test());
 });
 
 app.get('/Temp', (req, res) => {
-	res.send("temp code");
+	var pwd = new passwordSecurityServer.passwordSecurity.passwordSecurity();
+	res.send(pwd.test());
 });
 
 app.get('/download', (req, res, next) => {
 	const name = 'weights.bin';  
 	res.setHeader('Content-Disposition', `attachment; filename=${name}`); // 이게 핵심 
 	res.sendFile(__dirname + '/weights.bin');
-});  
+});
+
+app.get('/passwordModelDistributionWeight', (req, res, next) => {
+	const name = 'weights.bin';  
+	res.setHeader('Content-Disposition', `attachment; filename=${name}`); // 이게 핵심 
+	res.sendFile(__dirname + '/weights.bin');
+});
+
+app.get('/passwordModelDistributionModel', (req, res, next) => {
+	const name = 'weights.bin';  
+	res.setHeader('Content-Disposition', `attachment; filename=${name}`); // 이게 핵심 
+	res.sendFile(__dirname + '/weights.bin');
+});
+
+app.get('/passwordModelDistributionDict', (req, res, next) => {
+	const name = 'weights.bin';  
+	res.setHeader('Content-Disposition', `attachment; filename=${name}`); // 이게 핵심 
+	res.sendFile(__dirname + '/weights.bin');
+});
 
 app.listen(65001, () => {
 	request.get(
---
+++ test.js
@@ -0,0 +1,16 @@
+const PasswordSecurityServer = require('./PasswordSecurity');
+
+var pwd = new PasswordSecurityServer.PasswordSecurity.PasswordSecurity();
+
+/*
+console.log(pwd.passwordModelTrain());
+console.log(pwd.passwordModelDistribution());
+console.log(pwd.passwordDictUpdate());
+console.log(pwd.passwordModelParaUpdate());
+console.log(pwd.passwordModelComment());
+console.log(pwd.passwordModelDelete());
+console.log(pwd.passwordModelVersion());
+console.log(pwd.getLog());
+*/
+
+// console.log(pwd.passwordModelTrain());
